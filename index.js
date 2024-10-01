55ed0516bd0bd1709d635225668479b0add118e1
Illya <katinskyi.illia@lll.kpi.ua>
2024-10-01 23:18:42 +0300

    Handle process to correctly close the server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+let server;
+
 app.get('/healthcheck', (req, res) => {
   res.status(200).json({
     status: 'OK',
@@ -9,6 +11,22 @@ app.get('/healthcheck', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const shutdown = () => {
+  console.log('Received shutdown signal, closing server...');
+  server.close(() => {
+    console.log('HTTP server closed.');
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error('Forcing shutdown after 10 seconds...');
+    process.exit(1);
+  }, 10000);
+};
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
